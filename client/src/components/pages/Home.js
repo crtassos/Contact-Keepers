d496b45c8e2a0088504f720f3441f48a8d09f7c2
Tassos Vassilopoulos <nvrsr@hotmail.com>
2021-03-28 17:18:59 +0100

    1) Newly added components 2) Added react context 3) Added react reducer 4) React router and two routes 5) new packages: axios, react-router-dom, react-transition-group, uuid 6) vscode option compactFolders to false

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Contacts from '../contacts/Contacts'
+
+const Home = () => {
+    return (
+        <div className='grid-2'>
+            {/* Contact Form */}
+            <div>
+                <Contacts />
+            </div>
+        </div>
+    )
+}
+
+export default Home
