bb671a8081ac774a2eb21d0bcc5c6dfc6e776105
Tassos Vassilopoulos <nvrsr@hotmail.com>
2021-04-13 09:24:54 +0100

    1) deleteContact calback added to context 2) deleteContact reducer

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,7 +36,8 @@ const ContactState = props => {
                 phone : "333-333-333",
                 type  : "professional"
             }
-        ]
+        ],
+        current: null
     }
 
     const [state, dispatch] = useReducer(contactReducer, initialState)
@@ -54,8 +55,14 @@ const ContactState = props => {
     }
 
     // Set Current Contact
+    const setCurrent = contact => {
+        dispatch({type: SET_CURRENT, payload: contact})
+    }
 
     // Clear Current Contact
+    const clearCurrent = () => {
+        dispatch({type: CLEAR_CURRENT})
+    }
 
     // Update Contact
 
@@ -67,8 +74,11 @@ const ContactState = props => {
         <ContactContext.Provider 
          value={{
              contacts: state.contacts,
+             current: state.current,
              addContact,
-             deleteContact
+             deleteContact,
+             setCurrent,
+             clearCurrent
          }}>
             { props.children }   
         </ContactContext.Provider>
